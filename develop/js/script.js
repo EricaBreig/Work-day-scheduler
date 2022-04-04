634161bc013ee4bdf6952ed2146110e09d13ad98
Erica Breig <ericabreig@gmail.com>
2022-04-04 17:50:51 -0400

    messed with fonts and colors, began adding the local storage function

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,72 +1,161 @@
-//Today's date
-function getCurrentDate(){
-var currentDay = moment().format('dddd MMMM Do, YYYY');
-$("#currentDay").text("Today: " + currentDay);}
+//Today's date with assigned function to call on later
+function getCurrentDate() {
+  var currentDay = moment().format("dddd MMMM Do, YYYY");
+  $("#currentDay").text("Today: " + currentDay);
+}
 
-
-// Planner container variable
+// Planner container variable===============================================
 var dayPlanner = [
-    {
-        id: "0",
-        hour: "09",
-        time: "09",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "01",
-        time: "13",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "02",
-        time: "14",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "03",
-        time: "15",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "04",
-        time: "16",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "05",
-        time: "17",
-        meridiem: "pm",
-        reminder: ""
-    },
-]
+  {
+    id: "0",
+    hour: "09",
+    time: "09",
+    meridiem: "am",
+    reminder: "",
+  },
+  {
+    id: "1",
+    hour: "10",
+    time: "10",
+    meridiem: "am",
+    reminder: "",
+  },
+  {
+    id: "2",
+    hour: "11",
+    time: "11",
+    meridiem: "am",
+    reminder: "",
+  },
+  {
+    id: "3",
+    hour: "12",
+    time: "12",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "4",
+    hour: "01",
+    time: "13",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "5",
+    hour: "02",
+    time: "14",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "6",
+    hour: "03",
+    time: "15",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "7",
+    hour: "04",
+    time: "16",
+    meridiem: "pm",
+    reminder: "",
+  },
+  {
+    id: "8",
+    hour: "05",
+    time: "17",
+    meridiem: "pm",
+    reminder: "",
+  },
+];
+//FUNCTIONS=================================================================
+// Saves the data to the local storage 
+function saveReminders() {
+  localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+}
+
+// sets any data in localStorage to the view
+function displayReminders() {
+  dayPlanner.forEach(function (_thisHour) {
+    $(`#${_thisHour.id}`).val(_thisHour.reminder);
+  });
+}
+
+// // sets any existing localStorage data to the view if it exists
+// function init() {
+//   var storedDay = JSON.parse(localStorage.getItem("dayPlanner"));
+
+//   if (storedDay) {
+//     dayPlanner = storedDay;
+//   }
+
+//   saveReminders();
+//   displayReminders();
+// }
+
+// loads header date
+getCurrentDate();
+
+// creates the visuals for the scheduler body
+dayPlanner.forEach(function (thisHour) {
+  // creates timeblocks row
+  var hourRow = $("<form>").attr({
+    class: "row",
+  });
+  $(".container").append(hourRow);
+
+  // Time Field 
+  var hourField = $("<div>").text(`${thisHour.hour}${thisHour.meridiem}`).attr({
+    class: "col-md-2 hour",
+  });
+
+  // Scheduler Data
+  var hourPlan = $("<div>").attr({
+    class: "col-md-9 description p-0",
+  });
+  var planData = $("<textarea>");
+  hourPlan.append(planData);
+  planData.attr("id", thisHour.id);
+  if (thisHour.time < moment().format("Ha")) {
+    planData.attr({
+      class: "past",
+    });
+  } else if (thisHour.time === moment().format("Ha")) {
+    planData.attr({
+      class: "present",
+    });
+  } else if (thisHour.time > moment().format("Ha")) {
+    planData.attr({
+      class: "future",
+    });
+  }
+
+  // creates save button
+  var saveButton = $("<i class='far fa-save fa-lg'></i>");
+  var savePlan = $("<button>").attr({
+    class: "col-md-1 saveBtn",
+  });
+  savePlan.append(saveButton);
+  hourRow.append(hourField, hourPlan, savePlan);
+});
+
+// loads any existing localstorage data after components created
+init();
+
+// saves data to be used in localStorage
+$(".saveBtn").on("click", function (event) {
+  event.preventDefault();
+  var saveIndex = $(this)
+    .siblings(".description")
+    .children(".future")
+    .attr("id");
+  dayPlanner[saveIndex].reminder = $(this)
+    .siblings(".description")
+    .children(".future")
+    .val();
+  console.log(saveIndex);
+  saveReminders();
+  displayReminders();
+});
