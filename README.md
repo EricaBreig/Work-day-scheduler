a8d5b718a0c0c238c23087f43cf22e007025eb7b
Erica Breig <ericabreig@gmail.com>
2022-04-04 19:08:09 -0400

    fixed up JS, edited README

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 #  Assingment 5:  Work Day Scheduler
+For this assignment, we were tasked with making a simple work day planner using Third Party APIs.
 
 ### Deployed links:
-[Erica's Work Day Scheduler](www.putlinkhere.com)
+[Erica's Work Day Scheduler](https://ericabreig.github.io/hw-5-scheduler/)
 
 ## Description
+The planner allows the user to type in their plans for each hour of their work day.
+(I added extra hours to account for commuting and typical overtime hours.)
+When they hit the save button, it saves their input to the local file, even after refreshing the page.
+In this assignment, I learned to utiilize the following:
+- jQuery for simplified functions
+- Bootstrap for styling and formatting
+- Google fonts for styling
+
 
 ## Credit
 Myself, and Trilogy Education Services for providing the starter code.
--- develop/js/script.js
+++ develop/js/script.js
@@ -2,10 +2,50 @@
 // Establishing the current day via moment
 $(document).ready(function(){
   var currentDay = moment().format("dddd MMMM Do, YYYY");
-  $("#currentDay").text("Today: " + currentDay);
+  $("#currentDay").text("Today's Date: " + currentDay);
+  //establishing the click function for the blue save button
     $(".saveBtn").on("click", function(){
         var plannerText = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
+        //puts the items from the user into the local storage
         localStorage.setItem(time, plannerText);
 
     }); 
+    // gets the notes from the local storage and sets it onto the page
+    $("#6 .description").val(localStorage.getItem("6"));
+    $("#7 .description").val(localStorage.getItem("7"));
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
+    $("#19 .description").val(localStorage.getItem("19"));
+
+    // Function for changing the time and changing the colors accordingly
+    function timeChange(){
+        var currentHour = moment().hour(); // selects the current hour
+        
+        $('.time-block').each(function(){
+            var idTime = parseInt($(this).attr("id"));
+            if (idTime < currentHour){
+                $(this).addClass("past");
+            } else if (idTime === currentHour){
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            } else {$(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            }
+        })
+    }
+    timeChange();
+    var timeInterval = setInterval(timeChange, 1000);
+append.timeInterval;
+
+});
